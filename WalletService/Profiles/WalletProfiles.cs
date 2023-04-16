9c87e425e29ffde9f0cb7b16fe72317f1a04a5fe
irgifahlevi <irgifahlevi5@gmail.com>
2023-04-16 21:21:22 +0700

    membuat wallet profiles automapper

---
+++ WalletService/Profiles/WalletProfiles.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using WalletService.Dtos;
+using WalletService.Models;
+
+namespace WalletService.Profiles
+{
+    public class WalletProfiles : Profile
+    {
+        public WalletProfiles()
+        {
+            CreateMap<Wallet, ReadWalletDto>();
+            CreateMap<CreateWalletDto, Wallet>();
+        }
+    }
+}
